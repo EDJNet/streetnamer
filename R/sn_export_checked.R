0aaf4d2b512c2ac28ed19f7b9e1c0b48ab3ffe44
Giorgio Comai <g@giorgiocomai.eu>
2022-07-27 16:25:56 +0200

    supposedly functional sn_export_checked

--- R/sn_export_checked.R
+++ R/sn_export_checked.R
@@ -1,9 +1,12 @@
-#' Title
+#' Exports checked file for further analysis
 #'
 #' @param gisco_id Defaults to NULL. If given, a valid code starting with a two letter country code. Either `gisco_id` or `country` must be given.
 #' @param country Defaults to NULL. If given, must be a two-letter code. Returns data only for the given country.
 #' @param source Source for data. Valid values include "database" and "fixed_csv".
 #' @param fixed_folder Base location of folder where csv files with manual fixes are stored. They can be located in subfolders.
+#' @param unlist Defaults to FALSE. If TRUE, all data are unlisted in place, with values separated by a `;`.
+#' @param export_folder Defaults to `sn_data_export`, created if not existing, overwrite files by default. If set to TRUE, unlist is also automatically set to TRUE.
+#' @param export_csv Logical, defaults to FALSE. If TRUE, output file is stored as a CSV file in a local sub-folder as defined by `export_folder`.
 #'
 #' @return
 #' @export
@@ -14,13 +17,20 @@ sn_export_checked <- function(gisco_id = NULL,
                               source = "fixed_csv",
                               additional_properties = c("P39", "P509", "P140", "P611", "P411", "P241", "P410", "P97", "P607", "P27", "P172"),
                               include_image_credits = TRUE,
+                              unlist = FALSE,
                               fixed_folder = "sn_data_fixed",
+                              export_folder = "sn_data_export",
+                              export_csv = FALSE,
                               cache = NULL, 
                               language = tidywikidatar::tw_get_language(),
                               overwrite_cache = FALSE,
                               cache_connection = NULL,
                               disconnect_db = TRUE
                               ) {
+  
+  if (export_csv==TRUE) {
+    unlist <- TRUE
+  }
   
   connection_db <- tidywikidatar::tw_connect_to_cache(
     connection = cache_connection,
@@ -174,19 +184,71 @@ sn_export_checked <- function(gisco_id = NULL,
         dplyr::left_join(y = img_metadata_df %>%
                            dplyr::transmute(wikidata_id = .data$id, 
                                             imgage_attribution_required = .data$attribution_required,
+                                            image_copyrighted = .data$copyrighted,
+                                            iamge_restrictions = .data$restrictions,
                                             image_credit = .data$credit,
                                             image_artist = .data$artist, 
                                             image_license_short_name = .data$license_short_name,
-                                            image_license_url = .datalicense_url,
+                                            image_license_url = .data$license_url,
                                             image_usage_terms = .data$usage_terms
                            ), 
                          by = "wikidata_id")
     }
     
-    
+    if (unlist == TRUE) {
+      output_df <- processed_df %>%
+        dplyr::group_by(row_number) %>%
+        dplyr::mutate(
+          dplyr::across(
+            where(is.list),
+            function(x) {
+              stringr::str_c(unique(unlist(x)),
+                             collapse = "; "
+              )
+            }
+          )
+        ) %>%
+        dplyr::ungroup() %>% 
+        dplyr::mutate(
+          gender_label_combo = dplyr::case_when(is.na(sex_or_gender_label)==FALSE&sex_or_gender_label!="female"&sex_or_gender_label!="male" ~ as.character("other"),
+                                                is.na(sex_or_gender_label)==FALSE ~ sex_or_gender_label,
+                                                is.na(sex_or_gender_label)==TRUE ~ gender,
+                                                TRUE ~ gender)
+        )
+    } else {
+      output_df <- processed_df %>%
+        dplyr::group_by(row_number) %>%
+        
+
+        dplyr::mutate(
+          unlisted_gender = stringr::str_c(unique(unlist(sex_or_gender_label)),
+                                           collapse = "; "
+          )) %>% 
+        dplyr::ungroup() %>% 
+        dplyr::mutate(
+          gender_label_combo = dplyr::case_when(is.na(unlisted_gender)==FALSE&unlisted_gender!="female"&unlisted_gender!="male" ~ as.character("other"),
+                                                is.na(unlisted_gender)==FALSE ~ unlisted_gender,
+                                                is.na(unlisted_gender)==TRUE ~ gender,
+                                                TRUE ~ gender)
+        ) %>% 
+        dplyr::select(-.data$unlisted_gender)
+    }
+ 
     
     
     
   }
+  tidywikidatar::tw_disconnect_from_cache(
+    cache = cache,
+    cache_connection = connection_db,
+    disconnect_db = disconnect_db,
+    language = language
+  )
+  
+  if (export_to_csv == TRUE) {
+    
+  }
+  
+  output_df
   
 }
