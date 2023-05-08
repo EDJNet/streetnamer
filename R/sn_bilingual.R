7f14bbf2f1d4fa9332aeb8dd55dfee9fa6674acf
Giorgio Comai <g@giorgiocomai.eu>
2023-05-08 17:44:44 +0200

    more consistent support for cities in areas where primary language is non obvious (so far, Belgium and South Tyrol)

--- R/sn_bilingual.R
+++ R/sn_bilingual.R
@@ -2,42 +2,60 @@
 #'
 #' So far supported:
 #'
-#' - major Belgian cities
+#' - Belgian cities
 #' - South Tyrol
 #'
-#' @param gisco_id A gisco identifier
-#' @param languages Couple of languages. Supported: "french-flemish"; "italian-german"
+#' Please file an issue if you find issues.
 #'
-#' @return
+#' @param gisco_id A gisco identifier
+#' @param languages Couple of languages in the format "it_de" for cities where
+#'   the name of the street is written first in Italian, then there's an hypen,
+#'   and then in German; "de_it" if it's vice versa.
+#' @param lau_year Defaults to 2020 for internal consistency. Passed to `latlon2map::ll_osm_get_lau_streets()`.
+#'
+#' @return A data frame with two columns, `name` and `name_clean`. `name_clean`
+#'   should have the "street" part of a street name removed as well as the part 
+#'   in the secondary language.
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#'   if(interactive()) {
-#'     library("latlon2map")
-#'     
-#'     options(timeout = 60000) # big timeout, as big downloads needed 
-#'     
-#'     
-#'     ll_set_folder(path = fs::path(fs::path_home_r(),
-#'                                   "R",
-#'                                   "ll_data"))
-#'     
-#'     sn_get_clean_street_name_bilingual_df(gisco_id = "IT_021008")
-#'     sn_get_clean_street_name_bilingual_df(gisco_id = "IT_021051")
-#'     
-#'   }
+#' if (interactive()) {
+#'   library("latlon2map")
+#'
+#'   options(timeout = 60000) # big timeout, as big downloads needed
+#'
+#'
+#'   ll_set_folder(path = fs::path(
+#'     fs::path_home_r(),
+#'     "R",
+#'     "ll_data"
+#'   ))
+#' 
+#' sn_get_clean_street_name_bilingual_df(gisco_id = "IT_021008") %>% 
+#'   dplyr::slice_sample(n = 10)
+#' sn_get_clean_street_name_bilingual_df(gisco_id = "IT_021051") %>% 
+#'   dplyr::slice_sample(n = 10)
+#' 
+#' sn_get_clean_street_name_bilingual_df(gisco_id = "BE100") %>% 
+#'   dplyr::slice_sample(n = 10)
+#' sn_get_clean_street_name_bilingual_df(gisco_id = "BE_44021") %>% 
+#'   dplyr::slice_sample(n = 10)
+#' sn_get_clean_street_name_bilingual_df(gisco_id = "BE_62063") %>% 
+#'   dplyr::slice_sample(n = 10)
 #' }
-
+#' }
 sn_get_clean_street_name_bilingual_df <- function(gisco_id,
                                                   street_names_df = NULL,
-                                                  languages = NULL) {
+                                                  languages = NULL,
+                                                  lau_year = 2020) {
   if (is.null(street_names_df) == FALSE) {
     current_street_names_df <- street_names_df %>%
       dplyr::distinct(name)
   } else {
     current_street_names_df <- latlon2map::ll_osm_get_lau_streets(
       gisco_id = gisco_id,
+      year = lau_year,
       unnamed_streets = FALSE,
       streets_sf = NULL
     ) %>%
@@ -45,91 +63,65 @@ sn_get_clean_street_name_bilingual_df <- function(gisco_id,
       dplyr::distinct(name)
   }
 
-  if (is.null(languages) == FALSE) {
-    if (languages == "french-flemish") {
-      languages  <- "fr_fl"
-    }  else if (languages == "italian-german") {
-      languages  <- "it_de"
-    } else if (languages == "german-italian") {
-      languages  <- "de_it"
-    }
+  if (gisco_id %in% sn_bilingual_gisco_id$gisco_id) {
+    current_gisco_id <- gisco_id
+    languages <- sn_bilingual_gisco_id %>%
+      dplyr::filter(gisco_id == current_gisco_id) %>%
+      dplyr::pull(languages)
   } else {
-    if (gisco_id %in% c("BE100",
-                        "BE_21004",
-                        "BE_21015",
-                        "BE_21001",
-                        "BE_21012",
-                        "BE_21016")) {
-      languages  <- "fr_fl"
-    } else if (gisco_id %in% sn_bilingual_gisco_id$gisco_id) {
-      current_gisco_id <- gisco_id
-      languages <- sn_bilingual_gisco_id %>% 
-        dplyr::filter(gisco_id == current_gisco_id) %>% 
-        dplyr::pull(languages)
-    } else {
-      languages = "other"
-    }
+    languages <- "other"
   }
 
-  if (languages == "fr_fl") {
-    name_clean_df <- current_street_names_df %>%
-      tidyr::separate(col = name,
-                      into = c("French",
-                               "Flemish"),
-                      sep = " - ",
-                      remove = FALSE) %>%
-      dplyr::transmute(name,
-                       name_clean = French) %>%
-      dplyr::mutate(name_clean = sn_clean_street_name(
-        street_name = .data$name_clean,
-        country = "Belgium"
-      ))
-  } else if (languages == "de_it") {
-    name_clean_df <- current_street_names_df %>%
-      tidyr::separate(col = name,
-                      into = c("German",
-                               "Italian"),
-                      sep = " - ",
-                      remove = FALSE) %>%
-      dplyr::transmute(name,
-                       name_clean = German) %>%
-      dplyr::mutate(name_clean = sn_clean_street_name(
-        street_name = .data$name_clean,
-        country = "Germany"
-      ))
-  } else if (languages == "it_de") {
-    name_clean_df <- current_street_names_df %>%
-      tidyr::separate(col = name,
-                      into = c("Italian",
-                               "German"),
-                      sep = " - ",
-                      remove = FALSE) %>%
-      dplyr::transmute(name,
-                       name_clean = Italian) %>%
-      dplyr::mutate(name_clean = sn_clean_street_name(
-        street_name = .data$name_clean,
-        country = "Italy"
-      ))
-  } else {
+  if (languages == "other") {
     usethis::ui_warn("Doing standard name cleaning, as no custom rule for the given gisco_id is currently available.")
     name_clean_df <- current_street_names_df %>%
       dplyr::mutate(name_clean = sn_clean_street_name(
         street_name = name,
-        country = stringr::str_extract(string = gisco_id,
-                                       pattern = "[[:alpha:]][[:alpha:]]") %>%
+        country = stringr::str_extract(
+          string = gisco_id,
+          pattern = "[[:alpha:]][[:alpha:]]"
+        ) %>%
           stringr::str_to_upper()
       ))
+    return(name_clean_df)
   }
+
+  languages_v <- as.character(stringr::str_split(
+    string = languages,
+    pattern = "_",
+    simplify = TRUE,
+    n = 2
+  ))
+  name_clean_df <- current_street_names_df %>%
+    tidyr::separate(
+      col = name,
+      into = languages_v,
+      sep = " - ",
+      remove = FALSE
+    ) %>%
+    dplyr::select(1:2) %>%
+    dplyr::rename(name_clean = 2) %>%
+    dplyr::mutate(name_clean = sn_clean_street_name(
+      street_name = .data$name_clean,
+      country = sn_language_defaults_by_country %>%
+        dplyr::filter(language_code == languages_v[1]) %>%
+        dplyr::slice_head(n = 1) %>%
+        dplyr::pull(country)
+    ))
 
   output_df <- name_clean_df %>%
     dplyr::mutate(
-      name = name %>% 
-        stringr::str_replace_all(pattern = stringr::fixed("\\"),
-                                 replacement = " ") %>%
+      name = name %>%
+        stringr::str_replace_all(
+          pattern = stringr::fixed("\\"),
+          replacement = " "
+        ) %>%
         stringr::str_squish(),
-      name_clean = name_clean %>% 
-        stringr::str_replace_all(pattern = stringr::fixed("\\"),
-                                 replacement = " ") %>%
+      name_clean = name_clean %>%
+        stringr::str_replace_all(
+          pattern = stringr::fixed("\\"),
+          replacement = " "
+        ) %>%
         stringr::str_squish()
     )
 
--- data-raw/sn_bilingual_gisco_id.R
+++ data-raw/sn_bilingual_gisco_id.R
@@ -1,13 +1,13 @@
 ## code to prepare `sn_bilingual_gisco_id` dataset goes here
 library("dplyr", warn.conflicts = FALSE)
-lau_nuts_df <- readr::read_csv(file = "https://github.com/EDJNet/lau_centres/raw/main/lau_centres/lau_2020_nuts_2021_pop_2018_p_2_adjusted_intersection.csv")
+#lau_nuts_df <- readr::read_csv(file = "https://github.com/EDJNet/lau_centres/raw/main/lau_centres/lau_2020_nuts_2021_pop_2018_p_2_adjusted_intersection.csv")
 
-south_tyrol_lau_df <- lau_nuts_df %>% 
+south_tyrol_lau_df <- sn_lau_by_nuts %>% 
   dplyr::filter(nuts_3 == "ITH10") %>% 
   dplyr::arrange(dplyr::desc(population))
 
 # actually checked only top 10 most popolous cities in South Tyrol
-sn_bilingual_gisco_id <- south_tyrol_lau_df %>%
+south_tyrol_lau_by_language_df <- south_tyrol_lau_df %>%
   dplyr::select(gisco_id, country, lau_name) %>% 
   dplyr::mutate(languages = dplyr::if_else(
     condition = gisco_id %in% c(
@@ -17,5 +17,36 @@ sn_bilingual_gisco_id <- south_tyrol_lau_df %>%
     true =  "it_de",
     false = "de_it"))
 
+
+## Belgium / actually checked only top, rest based on nuts2 region
+provinces_Wallonia_df <- tidywikidatar::tw_get_property(id = "Q231", p = "P150")
+
+nuts2_wallonia_v <- provinces_Wallonia_df %>% 
+  dplyr::pull(value) %>% 
+  tidywikidatar::tw_get_p1(p = "P605") 
+
+wallonia_df <- sn_lau_by_nuts %>% 
+  dplyr::filter(nuts_2 %in% c(nuts2_wallonia_v, 
+                              c("BE100",
+                                "BE_21004",
+                                "BE_21015",
+                                "BE_21001",
+                                "BE_21012",
+                                "BE_21016"))) 
+
+
+belgium_by_language_df <- sn_lau_by_nuts %>% 
+  dplyr::select(gisco_id, country, lau_name) %>% 
+  dplyr::filter(country == "BE") %>% 
+  dplyr::mutate(languages = dplyr::if_else(
+    condition = gisco_id %in% wallonia_df$gisco_id,
+    true =  "fr_nl",
+    false = "nl_fr"))
+
+
+sn_bilingual_gisco_id <- dplyr::bind_rows(
+  south_tyrol_lau_by_language_df,
+  belgium_by_language_df
+)
 
 usethis::use_data(sn_bilingual_gisco_id, overwrite = TRUE)
